ecd9f0b49bbd9136f060fd26a81a2a2556a66022
ashprasad-invisiblio <ashish@invisibl.io>
2024-07-29 17:00:47 +0530

    Edit handled

--- src/Components/PostCreator.tsx
+++ src/Components/PostCreator.tsx
@@ -16,22 +16,25 @@ export const PostCreator: React.FC<PostCreatorProps> = ({
     <div className="border-2 border-[#35373B] rounded-md bg-[#27292D] px-[20px] py-[24px] mt-4 text-left">
       <div className="font-bold text-lg text-[#C5C7CA]">
         {readOnly && content ? (
-          <div className="flex items-center mb-4">
-            <img
-              src={content.avatar}
-              alt="avatar"
-              className="w-10 h-10 rounded-full mr-3"
-              width={44}
-              height={44}
-            />
-            <div>
-              <div className="text-[12px] font-normal text-[#C5C7CA] leading-[20px]">
-                {content.name}
-              </div>
-              <div className="text-[12px] font-normal text-[#7D7F81] leading-[20px]">
-                {content.time} {content?.isEdited ? " • Edited" : ""}
+          <div className="flex items-center justify-between mb-4">
+            <div className="flex items-center">
+              <img
+                src={content.avatar}
+                alt="avatar"
+                className="w-10 h-10 rounded-full mr-3"
+                width={44}
+                height={44}
+              />
+              <div>
+                <div className="text-[12px] font-normal text-[#C5C7CA] leading-[20px]">
+                  {content.name}
+                </div>
+                <div className="text-[12px] font-normal text-[#7D7F81] leading-[20px]">
+                  {content.time} {content?.isEdited ? " • Edited" : ""}
+                </div>
               </div>
             </div>
+            <div className="text-[#fff] cursor-pointer">...</div>
           </div>
         ) : (
           "Create post"
